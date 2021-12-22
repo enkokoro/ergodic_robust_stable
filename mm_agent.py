a6a36e0e2f4d76eb742bf1cfe8b1141ac76bfb01
Evelyn Kuo <evelynkuoev@gmail.com>
2021-12-22 17:37:33 -0500

    no legit change

--- mm_agent.py
+++ mm_agent.py
@@ -24,9 +24,10 @@ class MMAgent1(Agent):
             grad_f_k = self.ff[k]['df_k']
             mu_k = self.ff[k]['mu_k']
             c_k = c_k_prev[k]
-            S_k = c_k - mu_k # N*t*c_k - N*t*mu_k
-           
-            B_j += lambda_k * S_k * grad_f_k(x_prev)
+            s_k = c_k - mu_k # S_k = N*t*s_k = N*t*c_k - N*t*mu_k
+            # print("additional: ", lambda_k * s_k * grad_f_k(x_prev))
+            B_j += lambda_k * s_k * grad_f_k(x_prev)
+        # print("B_j: ", B_j)
 
         # B_j = 0 or agent is too near boundary (because we are using approximations, 
         #   may end up slipping outside) 
@@ -43,7 +44,7 @@ class MMAgent1(Agent):
                 B_j = np.zeros(n)
                 r_idx = 0
                 # r_idx = random.randrange(n)
-                B_j[r_idx] = 1
+                B_j[r_idx] = -1
                 
             else:
                 # aim towards center if not already at center
