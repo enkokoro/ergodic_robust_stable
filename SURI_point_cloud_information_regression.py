8e0aa92109322b81396cc22c4916c565e972b6e0
enkokoro <evelynkuoev@gmail.com>
2022-07-14 17:18:59 -0400

    point cloud gif

--- SURI_point_cloud_information_regression.py
+++ SURI_point_cloud_information_regression.py
@@ -9,27 +9,27 @@ MODEL NAME
 POINT CLOUD
 MEASUREMENT
 """
-# model_name = "cube"
-# def model_point_cloud(): # return numpy array dimenstion (n_points, 3)
-#     return cube_point_cloud(10) 
-# def train_data(): # return (numpy array dimenstion (n_points, 3), scalar array dimension (n_points,)
-#     return cube_train_data()
-# def measurement(data):
-#     return np.max(data, axis=1)
-
-# model_name = "sphere"
-model_name = "cone"
+model_name = "cube"
 def model_point_cloud(): # return numpy array dimenstion (n_points, 3)
-    # return pv.Sphere().points
-    return pv.Cone().points
+    return cube_point_cloud(10) 
 def train_data(): # return (numpy array dimenstion (n_points, 3), scalar array dimension (n_points,)
-    pc = model_point_cloud()
-    num_samples = min(10, len(pc)//2)
-    idx = random.sample(range(0, len(pc)), num_samples)
-    data_tr = pc[idx,:]
-    return data_tr, measurement(data_tr)
+    return cube_train_data()
 def measurement(data):
     return np.max(data, axis=1)
+
+# model_name = "sphere"
+# model_name = "cone"
+# def model_point_cloud(): # return numpy array dimenstion (n_points, 3)
+#     # return pv.Sphere().points
+#     return pv.Cone().points
+# def train_data(): # return (numpy array dimenstion (n_points, 3), scalar array dimension (n_points,)
+#     pc = model_point_cloud()
+#     num_samples = min(10, len(pc)//2)
+#     idx = random.sample(range(0, len(pc)), num_samples)
+#     data_tr = pc[idx,:]
+#     return data_tr, measurement(data_tr)
+# def measurement(data):
+#     return np.max(data, axis=1)
 
 """
 FUNCTION REGRESSION SPECIFIC INPUTS
@@ -95,12 +95,75 @@ plt.add_mesh(volume_te, scalars=val_te, cmap='viridis')
 pyvista_gif(plt, model_name, "test_on_model", volume_te.length)
 
 # ground truth for comparison
+val_gt = measurement(data_te)
 plt = pv.Plotter(off_screen=True)
-plt.add_mesh(volume_te, scalars=measurement(data_te), cmap='viridis')
+plt.add_mesh(volume_te, scalars=val_gt, cmap='viridis')
 pyvista_gif(plt, model_name, "ground_truth_on_model", volume_te.length)
+
+# error map
+error = abs(val_gt - val_te)
+plt = pv.Plotter(off_screen=True)
+plt.add_points(cloud_tr, color="white", render_points_as_spheres=True, point_size=20.0)
+plt.add_mesh(volume_te, scalars=error, cmap='binary')
+pyvista_gif(plt, model_name, "error_on_model", volume_te.length)
 
 # uncertainty on model
 plt = pv.Plotter(off_screen=True)
 plt.add_points(cloud_tr, color="white", render_points_as_spheres=True, point_size=20.0)
 plt.add_mesh(volume_te, scalars=std_te, cmap='binary')
 pyvista_gif(plt, model_name, "uncertainty_on_model", volume_te.length)
+
+
+# iterative
+
+for _i in range(len(data_tr)):
+    i = _i + 1
+    _data_tr = data_tr[:i,:]
+    _val_tr = val_tr[:i]
+    _cloud_tr = pv.PolyData(_data_tr)
+    model.fit(_data_tr, _val_tr)
+    _val_te, _std_te = model.predict(data_te, return_std=True)
+
+    if _i == 0:
+        info_clim = [np.min(val_gt), np.max(val_gt)]
+        print("info: ", info_clim)
+        uncertainty_clim = [0, np.max(abs(_std_te))]
+        print("uncertainty: ", uncertainty_clim)
+        error_clim = [0, np.max(abs(val_gt-_val_te))]
+        print("error: ", error_clim)
+    
+    info_map = pv.Plotter(off_screen=True)
+    info_map.add_title("Problematic Map", font_size=12)
+    info_map.show(auto_close=False)
+    info_map.open_gif(f"gaussianprocess/{model_name}_problem_map"+"_{:02d}.gif".format(i))
+
+    info_map.add_points(_cloud_tr, scalars=_val_tr, cmap='viridis', render_points_as_spheres=True, point_size=20.0)
+    info_map.add_mesh(volume_te, scalars=_val_te, cmap='viridis', clim=info_clim)
+    info_path = info_map.generate_orbital_path(n_points=36, shift=volume_te.length)
+    info_map.orbit_on_path(info_path, write_frames=True)
+
+    info_map.close()
+
+    uncertainty_map = pv.Plotter(off_screen=True)
+    uncertainty_map.add_title("Uncertainty Map", font_size=12)
+    uncertainty_map.show(auto_close=False)
+    uncertainty_map.open_gif(f"gaussianprocess/{model_name}_uncertainty_map"+"_{:02d}.gif".format(i))
+
+    uncertainty_map.add_points(_cloud_tr, color="white", render_points_as_spheres=True, point_size=20.0)
+    uncertainty_map.add_mesh(volume_te, scalars=_std_te, cmap='binary', clim=uncertainty_clim)
+    uncertainty_path = uncertainty_map.generate_orbital_path(n_points=36, shift=volume_te.length)
+    uncertainty_map.orbit_on_path(uncertainty_path, write_frames=True)
+
+    uncertainty_map.close()
+
+    error_map = pv.Plotter(off_screen=True)
+    error_map.add_title("Error Map", font_size=12)
+    error_map.show(auto_close=False)
+    error_map.open_gif(f"gaussianprocess/{model_name}_error_map"+"_{:02d}.gif".format(i))
+
+    error_map.add_points(_cloud_tr, color="white", render_points_as_spheres=True, point_size=20.0)
+    error_map.add_mesh(volume_te, scalars=abs(val_gt-_val_te), cmap='binary', clim=error_clim)
+    error_path = error_map.generate_orbital_path(n_points=36, shift=volume_te.length)
+    error_map.orbit_on_path(error_path, write_frames=True)
+
+    error_map.close()
